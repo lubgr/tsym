87a944ceb4b0280d2c8d12e55a8593d9e39362d0
Lukas Boeger <dev@lboeger.de>
2015-10-09 13:52:53 +0200

    fix argument lists in requirement script

--- bin/requirements.sh
+++ bin/requirements.sh
@@ -12,7 +12,7 @@ SUPPLEMENT_FILES="/usr/local/share/gdb/cpputest.py /usr/local/share/gdb/hlgdb.py
 function checkExecutable {
     for executable in $1; do
         if ! which $executable &> /dev/null; then
-            echo "$2 executable $executable not found!"
+            echo "$2 executable $executable not found"
         fi
     done
 }
@@ -20,7 +20,7 @@ function checkExecutable {
 function checkLibraryLink {
     for flag in $1; do
         if ! echo "int main() { return 0; }" | cc $flag -o /dev/null -xc++ - &>/dev/null; then
-            echo "Couldn't link against $2 ${flag}!"
+            echo "Couldn't link against $2 ${flag}"
         fi
     done
 }
@@ -29,7 +29,7 @@ function checkHeaderFiles {
     for header in $1; do
         if ! echo -e "#include \"$header\"\n int main() { return 0; }" | \
             cc $flag -o /dev/null -xc++ - ; then
-            echo "Couldn't include $2 header ${header}!"
+            echo "Couldn't include $2 header ${header}"
         fi
     done
 }
@@ -37,17 +37,17 @@ function checkHeaderFiles {
 function checkFiles {
     for file in $1; do
         if ! [ -f $file ]; then
-            echo "$2 file $file not found!"
+            echo "$2 file $file not found"
         fi
     done
 }
 
-checkExecutable $NECESSARY_EXEC "Mandatory"
-checkLibraryLink $NECESSARY_LDFLAGS "mandatory"
-checkHeaderFiles $NECESSARY_HEADERS "mandatory"
+checkExecutable "$NECESSARY_EXEC" "Mandatory"
+checkLibraryLink "$NECESSARY_LDFLAGS" "mandatory"
+checkHeaderFiles "$NECESSARY_HEADERS" "mandatory"
 
-checkExecutable $SUPPLEMENT_EXEC "Supplementary"
-checkLibraryLink $SUPPLEMENT_LDFLAGS "supplementary"
-checkHeaderFiles $SUPPLEMENT_HEADERS "supplementary"
-checkFiles $SUPPLEMENT_FILES "Supplementary"
+checkExecutable "$SUPPLEMENT_EXEC" "Supplementary"
+checkLibraryLink "$SUPPLEMENT_LDFLAGS" "supplementary"
+checkHeaderFiles "$SUPPLEMENT_HEADERS" "supplementary"
+checkFiles "$SUPPLEMENT_FILES" "Supplementary"
 
