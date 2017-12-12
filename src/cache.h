42ab159b40e0ff6b9ffeef7e6abc273ff488e59f
Lukas Böger <dev@lboeger.de>
2017-12-12 09:29:52 +0100

    give meaningful names to template types

--- src/cache.h
+++ src/cache.h
@@ -6,7 +6,7 @@
 #include <string>
 
 namespace tsym {
-    template<class S, class T, bool forceCaching = false> class Cache {
+    template<class Key, class Value, bool forceCaching = false> class Cache {
         /* Cache for key-value-pairs with std::hash and std::equal_to specializations. It serves as
          * a lookup-pool for expensive transformations and/or symbols. The latter enables usage of
          * the same Base object, when identical Symbol instances are created, which speeds up an
@@ -16,48 +16,48 @@ namespace tsym {
             Cache(const Cache& other) = delete;
             const Cache& operator = (const Cache& rhs) = delete;
 
-            const T& insertAndReturn(const S& key, const T& value)
+            const Value& insertAndReturn(const Key& key, const Value& value)
             {
                 rep.insert(std::make_pair(key, value));
 
                 return value;
             }
 
-            const T *retrieve(const S& key) const
+            const Value *retrieve(const Key& key) const
             {
                 const auto lookup = rep.find(key);
 
                 return lookup != rep.end() ? &lookup->second : nullptr;
             }
 
-            typename std::unordered_map<S, T>::const_iterator begin() const
+            typename std::unordered_map<Key, Value>::const_iterator begin() const
             {
                 return rep.begin();
             }
 
-            typename std::unordered_map<S, T>::const_iterator end() const
+            typename std::unordered_map<Key, Value>::const_iterator end() const
             {
                 return rep.end();
             }
 
         private:
-            std::unordered_multimap<S, T> rep;
+            std::unordered_multimap<Key, Value> rep;
     };
 
 #ifdef TSYM_NO_OPTIONAL_CACHE
-    template<class S, class T> class Cache<S, T, false> {
+    template<class Key, class Value> class Cache<Key, Value, false> {
         public:
             Cache() = default;
             ~Cache() = default;
             Cache(const Cache& other) = delete;
             const Cache& operator = (const Cache& rhs) = delete;
 
-            const T& insertAndReturn(const S&, const T& value)
+            const Value& insertAndReturn(const Key&, const Value& value)
             {
                 return value;
             }
 
-            const T *retrieve(const S&) const
+            const Value *retrieve(const Key&) const
             {
                 return nullptr;
             }
