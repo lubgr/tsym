0c8066d35f2c1872aa64c6c99cedaec5ae0cc94a
Lukas Böger <dev@lboeger.de>
2018-01-12 17:20:51 +0100

    use full namespace identifier in logging macro

--- src/logging.h
+++ src/logging.h
@@ -8,10 +8,10 @@
 #define TSYM_LOGGING_ARGS(...) { "tsym", (std::strrchr(__FILE__, '/') + 1), __LINE__,\
     sgfy::str(__VA_ARGS__) }
 
-#define TSYM_DEBUG(...) Logger::getInstance()->debug(TSYM_LOGGING_ARGS(__VA_ARGS__))
-#define TSYM_INFO(...) Logger::getInstance()->info(TSYM_LOGGING_ARGS(__VA_ARGS__))
-#define TSYM_WARNING(...) Logger::getInstance()->warning(TSYM_LOGGING_ARGS(__VA_ARGS__))
-#define TSYM_ERROR(...) Logger::getInstance()->error(TSYM_LOGGING_ARGS(__VA_ARGS__))
-#define TSYM_CRITICAL(...) Logger::getInstance()->critical(TSYM_LOGGING_ARGS(__VA_ARGS__))
+#define TSYM_DEBUG(...) tsym::Logger::getInstance()->debug(TSYM_LOGGING_ARGS(__VA_ARGS__))
+#define TSYM_INFO(...) tsym::Logger::getInstance()->info(TSYM_LOGGING_ARGS(__VA_ARGS__))
+#define TSYM_WARNING(...) tsym::Logger::getInstance()->warning(TSYM_LOGGING_ARGS(__VA_ARGS__))
+#define TSYM_ERROR(...) tsym::Logger::getInstance()->error(TSYM_LOGGING_ARGS(__VA_ARGS__))
+#define TSYM_CRITICAL(...) tsym::Logger::getInstance()->critical(TSYM_LOGGING_ARGS(__VA_ARGS__))
 
 #endif
