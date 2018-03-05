4d4a0646c0ba61b33f28eb8ae74e24fd11c904b9
Lukas Böger <dev@lboeger.de>
2018-03-05 15:44:21 +0100

    reduce number of travis jobs and outsource build/test into script

---
+++ bin/travis.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -e
+
+TESTEXEC="./build/runtests"
+export CPPFLAGS="-isystem ${BOOSTDIR} -I src"
+
+build() {
+    CXX=`echo $1 | sed 's/gcc/g++/' | sed 's/clang/clang++/'`
+    CC=$1
+    make -j 4 CXX="${CXX}" CC="${CC}" lib staticlib tests
+    cat build/buildinfo.h
+}
+
+buildAndTest() {
+    build $1
+    "${TESTEXEC}"
+}
+
+clean() {
+    make clean
+}
+
+if [ "${MODE}" = "RELEASE" ]; then
+    for compiler in ${COMPILER}; do
+        buildAndTest "${compiler}"
+        clean
+    done
+elif [ "${MODE}" = "PROFILING" ]; then
+    export PROFILE="--coverage"
+
+    buildAndTest "${COMPILER}"
+    lcov -c -d . -o build/coverage.info
+    lcov --remove build/coverage.info '*/include/*' '*/boost/*' -o build/filtered.info
+    test -f build/filtered.info && coveralls-lcov build/filtered.info
+    clean
+
+    export PROFILE="-pg"
+    buildAndTest "${COMPILER}"
+    gprof "${TESTEXEC}" gmon.out | head -n 100
+    clean
+elif [ "${MODE}" = "DEBUG" ]; then
+    export CXXFLAGS="-fsanitize=address -fno-omit-frame-pointer -O0 -g -std=c++14 -fPIC"
+    export CFLAGS="-fsanitize=address -fno-omit-frame-pointer -O0 -g -fPIC"
+    export LIBS=-lasan
+    buildAndTest "${COMPILER}"
+    clean
+
+    export CXXFLAGS="-O0 -g -std=c++14 -fPIC"
+    export CFLAGS="-O0 -g -fPIC"
+    unset LIBS
+    build "${COMPILER}"
+    valgrind --error-exitcode=1 --leak-check=full "${TESTEXEC}"
+    clean
+fi
