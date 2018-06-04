080b887412a9196d7e19d5fdf76d3c01eb41782f
Lukas Böger <dev@lboeger.de>
2018-06-04 22:21:58 +0200

    invoke clang-tidy in static analysis script

--- bin/staticcheck.sh
+++ bin/staticcheck.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+if [[ $# -ne 1 || ! -f $1 ]]; then
+    echo "Usage: $0 [compile command database (json)]"
+    exit 1
+fi
+
+CDB=$1
 CPPCHECK=cppcheck
+CLANGTIDY=clang-tidy
 CPPCLEAN=cppclean
 OCLINT=oclint
 FMTSCRIPT=./bin/format.sh
 CONFIGDIR=misc
 
-INCLUDE="-I src -I include/tsym -I test"
-SRC="src/ test/"
+INCLUDE="-I src -I include/tsym -I unit-tests"
+SRC="src/ unit-tests/"
 
 EXIT=0
 
@@ -15,11 +22,16 @@ if env | grep BOOSTDIR; then
     SYSTEM_INCLUDE="-isystem ${BOOSTDIR}"
 fi
 
+if ! which $CLANGTIDY &> /dev/null; then
+    echo "$CLANGTIDY not found"
+    exit 1
+fi
+
 if which $CPPCHECK &> /dev/null; then
     $CPPCHECK --template='{file} +{line} ({severity}/{id}): {message}' \
     -j 4 \
     --enable=all --suppressions-list=$CONFIGDIR/cppcheckrc \
-    -q $INCLUDE --suppress=missingIncludeSystem --std=c++14 --language=c++ $SRC
+    -q --project="${CDB}"
 else
     echo "$CPPCHECK not found"
     EXIT=1
@@ -39,11 +51,21 @@ fi
 echo ''
 
 if which $OCLINT &> /dev/null; then
-    $OCLINT -version && \
-    $OCLINT `find $SRC -iname '*.cpp' -or -iname '*.h'` -max-priority-3=0 -- -std=c++14 -x c++ $INCLUDE $SYSTEM_INCLUDE \
-    | grep -v '|P3\]'
+    $OCLINT `find $SRC -iname '*.cpp' -or -iname '*.h'` -max-priority-3=0 -- \
+        -std=c++14 -x c++ $INCLUDE $SYSTEM_INCLUDE | grep -v '|P3\]'
 else
     echo "$OCLINT not found"
+    EXIT=1
+fi
+
+echo ''
+
+if which $CLANGTIDY &> /dev/null; then
+    for file in `find ${SRC} -iname '*.cpp'`; do
+        $CLANGTIDY -quiet -p "${CDB}" "${file}"
+    done
+else
+    echo "$CLANGTIDY not found"
     EXIT=1
 fi
 
