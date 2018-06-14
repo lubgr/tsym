3c6735237212296c4004439cd581163c57b83356
Lukas Böger <dev@lboeger.de>
2018-06-14 23:35:28 +0200

    enhance debugging info for failing travis build

--- integration-tests/test-all.sh
+++ integration-tests/test-all.sh
@@ -27,7 +27,7 @@ fi
 
 mkdir -p "${TSYM_BUILD_DIR}"
 pushd "${TSYM_BUILD_DIR}"
-cmake -D CMAKE_INSTALL_PREFIX="${TSYM_INSTALLATION_PATH}" "${TSYM_CLONE_DIR}"
+cmake --debug-output -D CMAKE_INSTALL_PREFIX="${TSYM_INSTALLATION_PATH}" "${TSYM_CLONE_DIR}"
 make install
 popd
 
