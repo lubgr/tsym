16ac15cb0a0902af7878ec00a2719a581aed226a
Lukas Böger <dev@lboeger.de>
2018-09-27 21:27:34 +0200

    fix inline constexpr string literals by explicitly extern linkage

--- src/basetypestr.h
+++ src/basetypestr.h
@@ -3,14 +3,14 @@
 
 namespace tsym {
     namespace typestring {
-        inline constexpr const char* numeric = "Numeric";
-        inline constexpr const char* function = "Function";
-        inline constexpr const char* constant = "Constant";
-        inline constexpr const char* power = "Power";
-        inline constexpr const char* product = "Product";
-        inline constexpr const char* symbol = "Symbol";
-        inline constexpr const char* sum = "Sum";
-        inline constexpr const char* undefined = "Undefined";
+        extern const char* numeric;
+        extern const char* function;
+        extern const char* constant;
+        extern const char* power;
+        extern const char* product;
+        extern const char* symbol;
+        extern const char* sum;
+        extern const char* undefined;
     }
 }
 
