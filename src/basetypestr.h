474ab33db0c13abd6bf04652b0db5bca2991ef2f
Lukas Böger <dev@lboeger.de>
2018-09-28 12:55:42 +0200

    switch back to non-inline extern string literals to fix sanitizer builds

--- src/basetypestr.h
+++ src/basetypestr.h
@@ -3,14 +3,14 @@
 
 namespace tsym {
     namespace typestring {
-        extern inline constexpr const char* numeric = "Numeric";
-        extern inline constexpr const char* function = "Function";
-        extern inline constexpr const char* constant = "Constant";
-        extern inline constexpr const char* power = "Power";
-        extern inline constexpr const char* product = "Product";
-        extern inline constexpr const char* symbol = "Symbol";
-        extern inline constexpr const char* sum = "Sum";
-        extern inline constexpr const char* undefined = "Undefined";
+        extern const char* numeric;
+        extern const char* function;
+        extern const char* constant;
+        extern const char* power;
+        extern const char* product;
+        extern const char* symbol;
+        extern const char* sum;
+        extern const char* undefined;
     }
 }
 
