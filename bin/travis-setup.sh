945a89503b437a3e9f29992128c1a012ee0e8161
Lukas Böger <mail@lboeger.de>
2019-07-22 23:28:50 +0200

    install boost 1.70 headers and update cppcheck

--- bin/travis-setup.sh
+++ bin/travis-setup.sh
@@ -7,7 +7,7 @@ if [ "${MODE}" = "PROFILING" ]; then
 elif [ "${MODE}" = "ANALYSIS" ]; then
     sudo pip install --upgrade cppclean
 
-    CPPCHECKVER=1.82
+    CPPCHECKVER=1.88
     wget -qO- https://github.com/danmar/cppcheck/archive/${CPPCHECKVER}.tar.gz | tar xz
     sudo make -C ./cppcheck-${CPPCHECKVER} SRCDIR=build CFGDIR=/usr/share/cppcheck/cfg HAVE_RULES=yes install
 fi
