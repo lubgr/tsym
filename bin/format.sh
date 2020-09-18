cd5b48fa9f545df4d6520855c7253413afc91195
Lukas Böger <mail@lboeger.de>
2020-09-18 21:40:32 +0100

    remove outdated clang-format version check

--- bin/format.sh
+++ bin/format.sh
@@ -2,11 +2,6 @@
 
 CLANGFMT="clang-format --style=file"
 
-if ! $CLANGFMT --version | grep -qs '[5-9]\.[0-9]'; then
-    echo "Version requirement of clang format not met, >= 5 is required"
-    exit 1
-fi
-
 inplace=""
 files=`git ls-files -- '*.h' '*.cpp'`
 
